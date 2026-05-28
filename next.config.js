e67cae1a29d7b95585b89caf01f9a498bba733e2
MTVZ27 <mario19149601@iclud.com>
2026-05-28 17:59:36 +0300

    Optimize external product images with SmartImage

--- next.config.js
+++ next.config.js
@@ -8,6 +8,21 @@ checkEnvVariables()
 const S3_HOSTNAME = process.env.MEDUSA_CLOUD_S3_HOSTNAME
 const S3_PATHNAME = process.env.MEDUSA_CLOUD_S3_PATHNAME
 
+const PRODUCT_IMAGE_HOSTNAMES = [
+  "www.anglingdirect.co.uk",
+  "www.mftackle.com",
+  "nasluka-shop.com",
+  "fishingindustry.bg",
+  "s13emagst.akamaized.net",
+  "www.angel-domaene.de",
+  "foxcdn1.blob.core.windows.net",
+  "www.prestoninnovations.com",
+  "matchfishing.hr",
+  "hobbyhome.co.uk",
+  "cbu01.alicdn.com",
+]
+
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -26,6 +41,10 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [
+      ...PRODUCT_IMAGE_HOSTNAMES.map((hostname) => ({
+        protocol: "https",
+        hostname,
+      })),
       {
         protocol: "http",
         hostname: "localhost",
--- src/modules/common/components/smart-image/index.tsx
+++ src/modules/common/components/smart-image/index.tsx
@@ -1,25 +1,65 @@
 import Image, { ImageProps, StaticImageData } from "next/image"
 import React from "react"
 
-const isExternalUrl = (src: string | StaticImageData | any): src is string => {
-    return typeof src === "string" && /^https?:\/\//i.test(src)
+const OPTIMIZED_EXTERNAL_IMAGE_HOSTNAMES = new Set([
+  "www.anglingdirect.co.uk",
+  "www.mftackle.com",
+  "nasluka-shop.com",
+  "fishingindustry.bg",
+  "s13emagst.akamaized.net",
+  "www.angel-domaene.de",
+  "foxcdn1.blob.core.windows.net",
+  "www.prestoninnovations.com",
+  "matchfishing.hr",
+  "hobbyhome.co.uk",
+  "cbu01.alicdn.com",
+])
+
+const isExternalUrl = (src: string | StaticImageData | unknown): src is string => {
+  return typeof src === "string" && /^https?:\/\//i.test(src)
+}
+
+const isOptimizedExternalUrl = (src: string) => {
+  try {
+    const { hostname } = new URL(src)
+
+    return OPTIMIZED_EXTERNAL_IMAGE_HOSTNAMES.has(hostname.toLowerCase())
+  } catch {
+    return false
+  }
 }
 
 export default function SmartImage(props: ImageProps) {
-    const { src, alt, className, priority, loading, quality, fill, sizes, ...rest } = props
+  const {
+    src,
+    alt,
+    className,
+    priority,
+    loading,
+    quality,
+    fill,
+    sizes,
+    placeholder,
+    blurDataURL,
+    ...rest
+  } = props
 
-    if (isExternalUrl(src)) {
-        return (
-            <img
-                src={src as string}
-                alt={alt}
-                className={className}
-                loading={priority ? undefined : (loading || "lazy")}
-                decoding="async"
-                {...(rest as any)}
-            />
-        )
+  if (isExternalUrl(src)) {
+    if (isOptimizedExternalUrl(src)) {
+      return <Image {...props} />
     }
 
-    return <Image {...props} />
+    return (
+      <img
+        src={src}
+        alt={alt}
+        className={className}
+        loading={priority ? undefined : loading || "lazy"}
+        decoding="async"
+        {...(rest as React.ImgHTMLAttributes<HTMLImageElement>)}
+      />
+    )
+  }
+
+  return <Image {...props} />
 }
