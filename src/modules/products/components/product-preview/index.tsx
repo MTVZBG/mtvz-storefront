57a2452370cbc0b910268463388807edb676f6da
MTVZ27 <mario19149601@iclud.com>
2026-06-16 10:18:04 +0300

    Remove fake star ratings from product cards

--- src/modules/products/components/product-preview/index.tsx
+++ src/modules/products/components/product-preview/index.tsx
@@ -1,30 +1,17 @@
-import { listProducts } from "@lib/data/products"
 import { getProductPrice } from "@lib/util/get-product-price"
 import { HttpTypes } from "@medusajs/types"
 import LocalizedClientLink from "@modules/common/components/localized-client-link"
 import Thumbnail from "../thumbnail"
 import PreviewPrice from "./price"
 
-const StarRating = () => (
-  <div className="flex gap-[2px] text-black">
-    {[1, 2, 3, 4, 5].map((i) => (
-      <svg key={i} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4">
-        <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clipRule="evenodd" />
-      </svg>
-    ))}
-  </div>
-)
-
 export default async function ProductPreview({
   product,
   isFeatured,
-  region,
 }: {
   product: HttpTypes.StoreProduct
   isFeatured?: boolean
   region: HttpTypes.StoreRegion
 }) {
-
   const { cheapestPrice } = getProductPrice({
     product,
   })
@@ -58,7 +45,6 @@ export default async function ProductPreview({
       </div>
 
       <div className="flex flex-col gap-1.5 flex-1 px-1">
-        <StarRating />
         <h3 className="text-[#1a1a1a] font-medium text-sm sm:text-[15px] leading-snug line-clamp-2 h-10 mt-1" data-testid="product-title">
           {product.title}
         </h3>
@@ -84,4 +70,3 @@ export default async function ProductPreview({
     </LocalizedClientLink>
   )
 }
-
